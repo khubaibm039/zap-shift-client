52bad0c3e19ea1f222fe3d53027ff39c588ba7ea
khubaibm039 <khubaibm039@gmail.com>
2026-08-29 02:14:58 +0600

    search by name and email

--- src/Pages/Dashboard/UsersManagement/UsersManagement.jsx
+++ src/Pages/Dashboard/UsersManagement/UsersManagement.jsx
@@ -2,13 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { FaUserShield, FaUserSlash } from "react-icons/fa";
 import Swal from "sweetalert2";
+import { useState } from "react";
 
 const UsersManagement = () => {
     const axiosSecure = useAxiosSecure();
+    const [searchText, setSearchText] = useState('')
+
     const { refetch, data: users = [] } = useQuery({
-        queryKey: ["users"],
+        queryKey: ["users",searchText],
         queryFn: async () => {
-            const res = await axiosSecure.get("/users");
+            const res = await axiosSecure.get(`/users?searchText=${searchText}`);
             return res.data;
         },
     });
@@ -72,6 +75,30 @@ const UsersManagement = () => {
     return (
         <div>
             <h1 className="text-4xl">Manage User : {users.length}</h1>
+           
+            <label className="input">
+                <svg
+                    className="h-[1em] opacity-50"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24">
+                    <g
+                        strokeLinejoin="round"
+                        strokeLinecap="round"
+                        strokeWidth="2.5"
+                        fill="none"
+                        stroke="currentColor">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                    </g>
+                </svg>
+                <input
+                    onChange={(e)=> setSearchText(e.target.value)}
+                    type="search" 
+                    className="grow" 
+                    placeholder="Search" />
+                <kbd className="kbd kbd-sm">⌘</kbd>
+                <kbd className="kbd kbd-sm">K</kbd>
+            </label>
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
